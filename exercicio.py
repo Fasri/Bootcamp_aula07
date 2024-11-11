eb69740e794d77779fe88bb1fdbf7f9f2962249f
Fasri <106215404+Fasri@users.noreply.github.com>
2024-11-11 14:35:22 -0300

    execicio2

--- exercicio.py
+++ exercicio.py
@@ -1,10 +1,22 @@
 #Calcular Média de Valores em uma Lista
 
-def media(lista):
-    soma = 0
-    for n in lista:
-        soma = soma + n
-    media = soma / len(lista)
-    return media
+# def media(lista:list[float]) ->float:
+#     soma = 0
+#     for n in lista:
+#         soma = soma + n
+#     media = soma / len(lista)
+#     return media
 
-print(media([23,2,34,4,5]))
+# print(media([23,2,34,4,5]))
+
+
+# Filtrar Dados Acima de um Limite
+
+def filtrar(lista:list[float],limite:float) ->list[float]:
+    lista_filtrada = []
+    for n in lista:
+        if n > limite:
+            lista_filtrada.append(n)
+    return lista_filtrada
+
+print(filtrar([23,2,34,4,5],2))
