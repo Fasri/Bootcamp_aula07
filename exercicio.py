3b9ddbb622747891e6d7f0d9458c884bb62c50fa
Fasri <106215404+Fasri@users.noreply.github.com>
2024-11-11 14:37:40 -0300

    exercicio.py

--- exercicio.py
+++ exercicio.py
@@ -12,11 +12,22 @@
 
 # Filtrar Dados Acima de um Limite
 
-def filtrar(lista:list[float],limite:float) ->list[float]:
-    lista_filtrada = []
-    for n in lista:
-        if n > limite:
-            lista_filtrada.append(n)
-    return lista_filtrada
+# def filtrar(lista:list[float],limite:float) ->list[float]:
+#     lista_filtrada = []
+#     for n in lista:
+#         if n > limite:
+#             lista_filtrada.append(n)
+#     return lista_filtrada
 
-print(filtrar([23,2,34,4,5],2))
+# print(filtrar([23,2,34,4,5],2))
+
+#Contar Valores Únicos em uma Lista
+
+def contar_unicos(lista:list[float]) ->int:
+    lista_unicos = []
+    for n in lista:
+        if n not in lista_unicos:
+            lista_unicos.append(n)
+    return len(lista_unicos)
+
+print(contar_unicos([23,2,5,4,5]))
